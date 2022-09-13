058b40df72ca6d9598d2de8ef30c44eb4bcd1609
Diak <info@petrik.hu>
2022-09-13 15:36:32 +0200

    Feladat 14 szoveg bementese

---
+++ src/feladat14.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class feladat14 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Adja meg a szöveget: ");
+        String bekert = sc.nextLine();
+
+
+
+
+
+    }
+
+
+
+
+}
