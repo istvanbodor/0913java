e6db3d248d2b354ab8f90698b30509295d75868e
Diak <info@petrik.hu>
2022-09-13 16:19:56 +0200

    tomb feltoltese veletlen ertekekkel

--- src/feladat15.java
+++ src/feladat15.java
@@ -3,6 +3,10 @@ public class feladat15 {
     public static void main(String[] args) {
         double[] tomb = new double[25];
 
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i] = Math.random()*200 -100;
+        }
+
 
 
 
