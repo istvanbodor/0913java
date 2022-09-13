fcbc2ba59c5e32ed88b14e8f39850a44fdfb3331
Diak <info@petrik.hu>
2022-09-13 15:44:23 +0200

    Menupontok

--- src/feladat14.java
+++ src/feladat14.java
@@ -7,6 +7,24 @@ public class feladat14 {
         System.out.print("Adja meg a szöveget: ");
         String bekert = sc.nextLine();
 
+        String menuPont ="";
+
+        while (!menuPont.equals("f"))
+        {
+            System.out.println("Adja meg, hogy mit szeretne csinálni a szöveggel:");
+            System.out.println("\ta - Nagybetűssé alakít" );
+            System.out.println("\tb - Kisbetűssé alakít" );
+            System.out.println("\tc - Hossz lekérdezés" );
+            System.out.println("\td - Összehasonlít egy másik szöveggel" );
+            System.out.println("\te - A szöveg egy részét kiiratni" );
+            System.out.println("\tf - Kilép" );
+
+
+
+
+
+        }
+
 
 
 
